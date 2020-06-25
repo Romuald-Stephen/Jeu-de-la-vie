71ad401e6f56a68b4ee49478d6817c62607e0b0c
romualdtorres <65345476+romualdtorres@users.noreply.github.com>
2020-06-25 16:15:46 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,19 +1,23 @@
 import threading
-
 import pygame
 
+from PyQt5.QtWidgets import QMainWindow, QAction, QWidget, QVBoxLayout, QTabWidget, QPushButton, \
+    QInputDialog, QLineEdit, QTableWidget, QTableWidgetItem
 
-
-# Define some colors
-# from main import x, y
+#Définir les couleurs
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-class Gui(threading.Thread):
-    def __init__(self, x = 20, y = 10):
+#créé class Gui
+
+class Gui(threading.Thread,QMainWindow):
+
+
+
+    def __init__(self,x = 20,y = 40): #init taille da la grill
         threading.Thread.__init__(self)
         self.width = 20
         self.height = 20
@@ -22,13 +26,13 @@ class Gui(threading.Thread):
         self.x = x
         self.y = y
 
-
-        for row in range(20):
+        for row in range(self.x):
             # Add an empty array that will hold each cell
             # in this row
             self.grid.append([])
-            for column in range(40):
+            for column in range(self.y):
                 self.grid[row].append(0)  # Append a cell
+
 
     def updateCell(self, x, y, valeur):
 
@@ -39,17 +43,93 @@ class Gui(threading.Thread):
         pygame.init()
 
         # Set the HEIGHT and WIDTH of the screen
-        WINDOW_SIZE = [self.x*(self.width+self.margin)+self.margin, self.y*(self.height+self.margin)+self.margin,]
+        WINDOW_SIZE = [self.x*(self.width+self.margin)+self.margin, self.height*(self.y+self.margin)+self.margin,]
         screen = pygame.display.set_mode(WINDOW_SIZE)
 
         # Set title of screen
-        pygame.display.set_caption("Array Backed Grid")
+        pygame.display.set_caption("Jeu de la vie TORRES PREVOST")
 
         # Loop until the user clicks the close button.
         done = False
 
         # Used to manage how fast the screen updates
         clock = pygame.time.Clock()
+
+        def __init__(self):
+                super().__init__()
+                self.initUI()
+
+        def initUI(self):
+
+                menubar = self.menuBar()
+                fichierMenu = menubar.addMenu("Fichier")
+
+
+                recAct = QAction("Enregistrer", self)
+                recAct.triggered.connect(self.rec)
+                recAct.setShortcut('ctrl+S')
+                recAct.setStatusTip('Enregistrer un fichier')
+
+                fichierMenu.addAction(recAct)
+
+
+                self.setMinimumSize(1280, 720)
+
+                self.setWindowTitle('RAVI 2')
+
+                self.myWidget = MyTableWidget(self)
+
+                self.setCentralWidget(self.myWidget)
+
+                self.show()
+
+
+        def rec(self):
+                print("rec")
+
+        class MyTableWidget(QWidget):
+
+            def __init__(self, parent):
+                super(QWidget, self).__init__(parent)
+                self.layout = QVBoxLayout(self)
+
+                # Initialize tab screen
+                self.tabs = QTabWidget()
+                self.tab1 = QWidget()
+                self.tab2 = QWidget()
+
+                # Add tabs
+                self.tabs.addTab(self.tab1, "Onglet 1")
+
+                self.tab1.layout = QVBoxLayout(self)
+                openButton = QPushButton("Nom ?")
+                openButton.clicked.connect(self.openClick)
+
+                self.tab1.layout.addWidget(openButton)
+                self.tab1.setLayout(self.tab1.layout)
+                self.tab1.setStyleSheet("background-image: url(./banniere_23.jpg); background-attachment: fixed;")
+
+
+
+                saveButton = QPushButton("Sauvegarde")
+                saveButton.clicked.connect(self.saveClick)
+                self.tab2.layout.addWidget(saveButton)
+
+                self.tab2.setLayout(self.tab2.layout)
+
+                # Add tabs to widget
+                self.layout.addWidget(self.tabs)
+                self.setLayout(self.layout)
+
+            # Dictionnaire
+            def saveClick(self):
+                print("save")
+
+                # save Tableau Onglet 2
+                import json
+                with open("data.json", "w") as info_personnelle:
+                    json.dump(dictionnaire, info_personnelle)
+
 
         # -------- Main Program Loop -----------
 
@@ -96,4 +176,3 @@ class Gui(threading.Thread):
         # Be IDLE friendly. If you forget this line, the program will 'hang'
         # on exit.
         pygame.quit()
-
